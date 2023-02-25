71734e9713389e220c2a890e25b6a49bf58ce2e3
m-rnd <markus.reinhold1337@gmail.com>
2023-02-25 13:38:31 +0100

    Initial Commit

---
+++ fi-news-ios/fi_news_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fi_news_iosApp.swift
+//  fi-news-ios
+//
+//  Created by Markus Reinhold on 25.02.23.
+//
+
+import SwiftUI
+
+@main
+struct fi_news_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
